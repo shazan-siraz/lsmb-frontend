af391d65ad1937c712795d75b2072f41b5f33202
unknown <siraj015213@gmail.com>
2024-10-11 17:50:36 +0600

    working header

--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -1,3 +1,4 @@
+import './Header.css'
 import { useDispatch } from "react-redux";
 import { logOut } from "../../redux/features/auth/authSlice";
 
@@ -10,8 +11,16 @@ const Header = () => {
 
   return (
     <div>
-      <div className="bg-[#092e3a] text-center py-4 text-white w-full top-0 fixed left-0 flex justify-around">
-        <h2>This is Header</h2>
+      <div className="bg-[#092e3a] text-center py-2 px-10 text-white w-full top-0 fixed left-0 flex justify-between">
+        
+        <div className="flex gap-2 md:ml-[250px]">
+          <p className="header-btn transition-all duration-300 ease-in-out">Transaction</p>
+          <p className="header-btn transition-all duration-300 ease-in-out">Savings TXN</p>
+          <p className="header-btn transition-all duration-300 ease-in-out">Loan TXN</p>
+          <p className="header-btn transition-all duration-300 ease-in-out">DPS TXN</p>
+          <p className="header-btn transition-all duration-300 ease-in-out">Today TXN</p>
+        </div>
+
         <div onClick={handleLogOut}>
           <button>Log Out</button>
         </div>
